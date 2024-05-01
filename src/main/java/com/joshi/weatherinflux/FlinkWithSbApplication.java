2abe80cb39ade23fbe0d9ef618dfdbf1a77d991c
Sujit Joshi <joshi.sujit020386@gmail.com>
2024-05-01 16:30:34 -0400

    WIP: Flink changes WITH Spring Boot

---
+++ src/main/java/com/joshi/weatherinflux/FlinkWithSbApplication.java
@@ -0,0 +1,12 @@
+package com.joshi.weatherinflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkWithSbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlinkWithSbApplication.class, args);
+  }
+}
